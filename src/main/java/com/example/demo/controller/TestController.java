0b789e312088efe6216bba0ad380fc4f2e1ebb04
dra2sun7 <dra2sun7@naver.com>
2023-09-20 15:59:35 +0900

    third commit 2

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,13 +1,31 @@
 package com.example.demo.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demo.service.KubernetesService;
+
+import java.util.List;
 
 @Controller
 public class TestController {
+    private final KubernetesService kubernetesService;
+
+    public TestController(KubernetesService kubernetesService) {
+        this.kubernetesService = kubernetesService;
+    }
 
     @RequestMapping(value = "/home")
     public String home(){
-        return "hello.html";
+        return "hello";
+    }
+
+    @PostMapping(value = "/printNodes")
+    public String printNodes(@RequestParam String apiserver, @RequestParam String token, Model model) {
+        List<String> nodeNames = kubernetesService.getNodeNames(apiserver, token);
+        model.addAttribute("nodeNames", nodeNames);
+        return "hello";
     }
 }
--- src/main/java/com/example/demo/service/KubernetesService.java
+++ src/main/java/com/example/demo/service/KubernetesService.java
@@ -1,2 +1,33 @@
-package com.example.demo.service;public class KubernetesService {
+package com.example.demo.service;
+
+import io.fabric8.kubernetes.api.model.Node;
+import io.fabric8.kubernetes.api.model.NodeList;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class KubernetesService {
+    public List<String> getNodeNames(String apiserver, String token) {
+        Config config = new ConfigBuilder()
+                .withMasterUrl(apiserver)
+                .withOauthToken(token)
+                .build();
+
+        KubernetesClient kubernetesClient = new DefaultKubernetesClient(config);
+
+        NodeList nodeList = kubernetesClient.nodes().list();
+        List<String> nodeNames = new ArrayList<>();
+
+        for (Node node : nodeList.getItems()) {
+            nodeNames.add(node.getMetadata().getName());
+        }
+
+        return nodeNames;
+    }
 }
