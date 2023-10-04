128071497dffa2fe2eab2d018759409f98c33c64
dra2sun7 <121752839+dra2sun7@users.noreply.github.com>
2023-10-04 19:25:49 +0900

    1004 모든 node에 job을 배포하고 로그를 spring 콘솔에 출력

--- src/main/java/com/example/demo/service/KubernetesService.java
+++ src/main/java/com/example/demo/service/KubernetesService.java
@@ -2,6 +2,9 @@ package com.example.demo.service;
 
 import io.fabric8.kubernetes.api.model.Node;
 import io.fabric8.kubernetes.api.model.NodeList;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.kubernetes.api.model.batch.v1.Job;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
@@ -36,14 +39,31 @@ public class KubernetesService {
                     String nodeName = node.getMetadata().getName();
                     String yamlTemplate = loadYamlTemplateFromFile();
                     String yamlContent = yamlTemplate.replace("${nodeName}", nodeName);
-                    System.out.println("yaml 파일의 내용 : \n" + yamlContent);
                     InputStream inputStream = new ByteArrayInputStream(yamlContent.getBytes());
 
                     kubernetesClient.load(inputStream).create();
-                    System.out.println("Job created successfully.");
+                    System.out.println("==============   " + nodeName+"Job created successfully.   ==============");
                     inputStream.close();
+
+                    Thread.sleep(60000); // Sleep for one minute
+
+                    PodList podList = kubernetesClient.pods().inNamespace("default").list();
+                    String log = null;
+                    for (Pod pod : podList.getItems()) {
+                        String podName = pod.getMetadata().getName();
+                        System.out.println(nodeName+"kubebench");
+                        if (podName.startsWith(nodeName+"kubebench")) {
+                            log = kubernetesClient.pods().inNamespace("default").withName(podName).getLog();
+                            System.out.println(log);
+                            break;
+                        }
+                    }
+
                 }
+
             } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         } catch (KubernetesClientException e) {
