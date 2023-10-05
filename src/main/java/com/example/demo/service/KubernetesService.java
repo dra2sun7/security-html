d7e721638b9b6149e8eb27ca067a39a3bb68cc0d
dra2sun7 <121752839+dra2sun7@users.noreply.github.com>
2023-10-05 12:17:04 +0900

    1005 delete, Complete check

--- src/main/java/com/example/demo/service/KubernetesService.java
+++ src/main/java/com/example/demo/service/KubernetesService.java
@@ -5,6 +5,7 @@ import io.fabric8.kubernetes.api.model.NodeList;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.api.model.batch.v1.Job;
+import io.fabric8.kubernetes.api.model.batch.v1.JobStatus;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
@@ -45,7 +46,7 @@ public class KubernetesService {
                     System.out.println("==============   " + nodeName+"Job created successfully.   ==============");
                     inputStream.close();
 
-                    Thread.sleep(60000); // Sleep for one minute
+                    waitForJobCompletion(kubernetesClient, nodeName+"kubebench");
 
                     PodList podList = kubernetesClient.pods().inNamespace("default").list();
                     String log = null;
@@ -57,12 +58,9 @@ public class KubernetesService {
                             break;
                         }
                     }
-
+                    deleteJob(kubernetesClient,nodeName+"kubebench");
                 }
-
             } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         } catch (KubernetesClientException e) {
@@ -75,6 +73,47 @@ public class KubernetesService {
         try (InputStream inputStream = getClass().getResourceAsStream("/application.yml")) {
             byte[] bytes = StreamUtils.copyToByteArray(inputStream);
             return new String(bytes, StandardCharsets.UTF_8);
+        }
+    }
+    private void waitForJobCompletion(KubernetesClient kubernetesClient, String jobName) {
+        long timeoutMillis = 5 * 60 * 1000; // 최대 대기 시간 (예: 10분)
+        long pollingIntervalMillis = 5000; // 상태 확인 간격 (예: 5초)
+
+        long startTime = System.currentTimeMillis();
+        boolean jobCompleted = false;
+
+        while (!jobCompleted && System.currentTimeMillis() - startTime < timeoutMillis) {
+            Job job = kubernetesClient.batch().v1().jobs().inNamespace("default").withName(jobName).get();
+            if (job != null) {
+                JobStatus jobStatus = job.getStatus();
+                if (jobStatus != null && jobStatus.getSucceeded() != null && jobStatus.getSucceeded() > 0) {
+                    jobCompleted = true;
+                }
+            }
+
+            if (!jobCompleted) {
+                try {
+                    Thread.sleep(pollingIntervalMillis); // 일정 간격으로 상태 확인
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+
+        if (jobCompleted) {
+            System.out.println("Job completed successfully.");
+        } else {
+            System.err.println("Timeout waiting for Job completion.");
+        }
+    }
+    private void deleteJob(KubernetesClient kubernetesClient, String jobName) {
+        try {
+            kubernetesClient.batch().v1().jobs().inNamespace("default").withName(jobName).delete();
+
+            System.out.println("Job deleted successfully.");
+        } catch (KubernetesClientException e) {
+            e.printStackTrace();
+            System.err.println("Error while deleting Job.");
         }
     }
 }
